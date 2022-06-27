ba9b1001b64d72bb72040a79b6c798e60a5abc90
eunjinn05 <eunjinn95@gmail.com>
2022-06-27 23:18:49 +0900

    [ADD] login 폼 작업

--- application/views/default.php
+++ application/views/default.php
@@ -7,6 +7,7 @@
 		<!--  기본 css	-->	
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
 		<link href="/assets/reset.css" rel="stylesheet">
+		<link href="/assets/default.css" rel="stylesheet">
 
 		<!--  기본 js	-->	
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -16,47 +17,46 @@
 
 		<!-- 추가 css -->
 		<? if (@is_file(ASSETSPATH."/".$fetch_method.".css")): ?>
-			<link href="<?=ASSETSPATH.'/'.$fetch_method?>.css" rel="stylesheet">
+			<link href="/<?=ASSETSPATH.'/'.$fetch_method?>.css" rel="stylesheet">
 		<?endif;?>
 
 	</head>
   <body>
-	<header>
-		<div class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-			<div class="container-fluid">
-			<a class="navbar-brand" href="/">TRIP</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarCollapse">
-				<ul class="navbar-nav me-auto mb-2 mb-md-0">
-				<li class="nav-item">
-					<a class="nav-link active" aria-current="page" href="#">Home</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Link</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link disabled">Disabled</a>
-				</li>
-				</ul>
-				<form class="d-flex" role="search">
-				<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-				<button class="btn btn-outline-success" type="submit">Search</button>
-				</form>
+		<header>
+			<div class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+				<div class="container-fluid">
+				<a class="navbar-brand" href="/">TRIP</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarCollapse">
+					<ul class="navbar-nav me-auto mb-2 mb-md-0">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Link</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled">Disabled</a>
+					</li>
+					</ul>
+					<form class="d-flex" role="search">
+					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success" type="submit">Search</button>
+					</form>
+				</div>
+				</div>
 			</div>
-			</div>
+		</header>
+
+		<div class=" m-auto">
+			<? include $fetch_views;?>
+
+			<? if (@is_file(VIEWPATH."/footer.php")) :
+				include VIEWPATH."/footer.php";
+			endif; ?>
 		</div>
-	</header>
-
-	<main>
-		<? include $fetch_views;?>
-		<footer class="container">
-			<p class="float-end"><a href="#">Back to top</a></p>
-			<p>© 2017–2022 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
-		</footer>
-	</main>
-
 	<!-- 추가 js -->
 	<? if (@is_file(ASSETSPATH."/".$fetch_method.".js")): ?>
 		<script src="<?=ASSETSPATH.'/'.$fetch_method?>.js"></script>
---
+++ application/views/footer.php
@@ -0,0 +1,3 @@
+<footer class="container">
+  <p>© 2017–2022 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+</footer>
